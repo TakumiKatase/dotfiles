0b4fd0d5fdb2b3fe97edbee7c4dce994d987fd28
devoc09 <b1816112@planet.kanazawa-it.ac.jp>
2020-11-13 01:29:26 +0900

    enable imwheel script

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
-ln -sf ~/dotfiles/.imwheelrc ~/.imwheelrc
+# ln -sf ~/dotfiles/.imwheelrc ~/.imwheelrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
