83e311384563e16ea38bc4ba53384c3143be91ef
devoc09 <b1816112@planet.kanazawa-it.ac.jp>
2020-12-12 23:56:23 +0900

    edit(dotfilesLink.sh): Add Symbol-Link FuzzyFinder for vim

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,3 +4,4 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 # ln -sf ~/dotfiles/.imwheelrc ~/.imwheelrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/alacritty.yml ~/.config/alacritty/
+ln -sf ~/dotfiles/VimFiles/ ~/.config/nvim/
