340965b3679e745fd2954f847b7250d30d380ea9
devoc09 <b1816112@planet.kanazawa-it.ac.jp>
2021-02-09 15:50:04 +0900

    ci(dotfilesLink.sh): add symbol for neovim

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,5 +4,6 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 # ln -sf ~/dotfiles/.imwheelrc ~/.imwheelrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/alacritty.yml ~/.config/alacritty/
+ln -sf ~/dotfiles/.init.vim ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/VimFiles/ ~/.config/nvim/
 ln -sf ~/dotfiles/terminals/kitty.conf ~/.config/kitty/kitty.conf
