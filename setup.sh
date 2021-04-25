3c9ef24ce9fe0dab312fc20dcf76d49242a87e9d
devoc09 <b1816112@planet.kanazawa-it.ac.jp>
2021-04-26 06:51:52 +0900

    refactor: add command for mkdir to setup script.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/tmux/ ~/.tmux && ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+mkdir -p ~/.config/alacritty/
 ln -sf ~/dotfiles/terminals/alacritty.yml ~/.config/alacritty/
+mkdir -p ~/.config/nvim
 ln -sf ~/dotfiles/.init.vim ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/zsh ~/.zsh && ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
