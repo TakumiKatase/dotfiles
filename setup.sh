c2633c63ce73026acce6b7314e9c1a942179b016
devoc09 <b1816112@planet.kanazawa-it.ac.jp>
2021-04-26 17:10:22 +0900

    refactor: update setup script.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/tmux/ ~/.tmux && ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/tmux/tmux-onedark-theme ~/.tmux/tmux-onedark-theme
 mkdir -p ~/.config/alacritty/
 ln -sf ~/dotfiles/terminals/alacritty.yml ~/.config/alacritty/
 mkdir -p ~/.config/nvim
 ln -sf ~/dotfiles/.init.vim ~/.config/nvim/init.vim
-ln -sf ~/dotfiles/zsh ~/.zsh && ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/zsh/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+ln -sf ~/dotfiles/zsh/zsh-history-substring-search ~/.zsh/zsh-history-substring-search
+ln -sf ~/dotfiles/zsh/zsh-syntax-highlighting ~/.zsh/zsh-syntax-highlighting
